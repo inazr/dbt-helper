388449ae83f19c4e81aca1cf7c2f26a0617cf836
stephanohm <github@stephan-ohm.com>
2026-05-27 10:46:44 +0200

    Host shared bar and inner tabs in a single tool-window Content

--- src/main/kotlin/com/dbthelper/toolwindow/DbtToolWindowFactory.kt
+++ src/main/kotlin/com/dbthelper/toolwindow/DbtToolWindowFactory.kt
@@ -1,6 +1,5 @@
 package com.dbthelper.toolwindow
 
-import com.dbthelper.core.DbtProjectLocator
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
@@ -10,19 +9,10 @@ import com.intellij.ui.content.ContentFactory
 class DbtToolWindowFactory : ToolWindowFactory, DumbAware {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val contentFactory = ContentFactory.getInstance()
-
-        val lineageTab = LineageTab(project, toolWindow.disposable)
-        val lineageContent = contentFactory.createContent(lineageTab, "Lineage", false)
-        toolWindow.contentManager.addContent(lineageContent)
-
-        val runnerTab = DbtRunnerTab(project, toolWindow.disposable)
-        val runnerContent = contentFactory.createContent(runnerTab, "Runner", false)
-        toolWindow.contentManager.addContent(runnerContent)
-
-        val statusPanel = DbtToolWindowPanel(project, toolWindow.disposable)
-        val statusContent = contentFactory.createContent(statusPanel, "Status", false)
-        toolWindow.contentManager.addContent(statusContent)
+        val mainPanel = DbtMainPanel(project, toolWindow.disposable)
+        val content = ContentFactory.getInstance().createContent(mainPanel, "", false)
+        content.isCloseable = false
+        toolWindow.contentManager.addContent(content)
     }
 
     override fun shouldBeAvailable(project: Project): Boolean = true
